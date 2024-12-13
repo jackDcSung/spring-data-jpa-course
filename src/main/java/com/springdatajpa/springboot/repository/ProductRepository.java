834ad6d0e93fddff74a206edcaef104c709dc623
ASUS VivoBook <likerockman102@hotmail.com>
2024-12-13 11:43:44 +0800

    Find by Single Field Name

--- src/main/java/com/springdatajpa/springboot/repository/ProductRepository.java
+++ src/main/java/com/springdatajpa/springboot/repository/ProductRepository.java
@@ -4,11 +4,16 @@ package com.springdatajpa.springboot.repository;
 import com.springdatajpa.springboot.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 
     public Product findByName(String name);
+
+
+    Optional<Product> findById(Long id);
 
 
 }
--- src/test/java/com/springdatajpa/springboot/respository/QueryMethodTest.java
+++ src/test/java/com/springdatajpa/springboot/respository/QueryMethodTest.java
@@ -10,29 +10,45 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class QueryMethodTest {
 
 
-@Autowired
-private ProductRepository productRepository;
+    @Autowired
+    private ProductRepository productRepository;
 
 
+    @Test
+    void findByNameMethod() {
+
+        Product product = productRepository.findByName("product 2");
+
+        System.out.println(product.getId());
+        System.out.println(product.getName());
+
+        System.out.println(product.getDescription());
+
+
+    }
 @Test
-void findByNameMethod(){
+void findByIdMethod(){
 
-    Product product=productRepository.findByName("product 2");
+
+  Product product=productRepository.findById(1L).get();
 
     System.out.println(product.getId());
+
     System.out.println(product.getName());
+
 
     System.out.println(product.getDescription());
 
 
 
+
+
+
+
+
+
+
 }
-
-
-
-
-
-
 
 
 
