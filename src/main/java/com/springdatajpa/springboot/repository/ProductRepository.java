c6645d143074c9e1e7c6862e2d09703d654520bd
ASUS VivoBook <likerockman102@hotmail.com>
2024-12-16 13:48:44 +0800

    Find by Multiple Field Names

--- src/main/java/com/springdatajpa/springboot/repository/ProductRepository.java
+++ src/main/java/com/springdatajpa/springboot/repository/ProductRepository.java
@@ -4,6 +4,7 @@ package com.springdatajpa.springboot.repository;
 import com.springdatajpa.springboot.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -14,6 +15,22 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
 
     Optional<Product> findById(Long id);
+
+    List<Product> findByNameOrDescription(String name, String description);
+
+
+
+
+
+    List<Product> findByNameAndDescription(String name, String description);
+
+
+
+
+
+
+
+
 
 
 }
--- src/test/java/com/springdatajpa/springboot/respository/QueryMethodTest.java
+++ src/test/java/com/springdatajpa/springboot/respository/QueryMethodTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
 public class QueryMethodTest {
 
@@ -26,29 +28,67 @@ public class QueryMethodTest {
 
 
     }
-@Test
-void findByIdMethod(){
+
+    @Test
+    void findByIdMethod() {
 
 
-  Product product=productRepository.findById(1L).get();
+        Product product = productRepository.findById(1L).get();
 
-    System.out.println(product.getId());
+        System.out.println(product.getId());
 
-    System.out.println(product.getName());
-
-
-    System.out.println(product.getDescription());
+        System.out.println(product.getName());
 
 
+        System.out.println(product.getDescription());
 
 
-
-
+    }
 
 
 
 
-}
+    @Test
+    void findByNameOrDescriptionMethod() {
+
+        List<Product> products=productRepository.findByNameOrDescription("product 1",
+                "product 1 description"
+                );
+
+
+
+        products.forEach(p->{
+
+            System.out.println(p.getId());
+            System.out.println(p.getName());
+
+        });
+
+
+    }
+
+
+
+    @Test
+    void findByNameAndDescriptionMethod() {
+
+        List<Product> products=productRepository.findByNameAndDescription("product 1",
+                "product 1 description"
+        );
+
+
+
+        products.forEach(p->{
+
+            System.out.println(p.getId());
+            System.out.println(p.getName());
+
+        });
+
+
+    }
+
+
 
 
 
